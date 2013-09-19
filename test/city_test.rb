142ae857cd68be6b6f6d0a98cdea7ec6836b026f
Tyler Long <tyler.stephen.long@gmail.com>
2013-09-18 22:02:27 -0600

    updated city test

--- test/city_test.rb
+++ test/city_test.rb
@@ -10,4 +10,10 @@ def test_it_does_not_return_nil
   ci = City.new
   assert !ci.city_name.nil?
   end
+
+  def test_it_should_compare_city_name
+    city = City.new("Atlanta")
+    city2= City.new("Chicago")
+    assert city < city2
+  end
 end
